2b5d579665c9c603967d58bb4ed188559b4548b0
w1r2s <donat.wirtus007@mail.ru>
2026-04-13 22:30:55 +0300

    Добавил runtime-модели заказа: OrderStatus, OrderItem и ActiveOrder

---
+++ Assets/Scripts/Order/OrderItem.cs
@@ -0,0 +1,37 @@
+﻿using Assets.Scripts.ScriptableObjects;
+
+namespace Assets.Scripts.Order
+{
+    public class OrderItem
+    {
+        private readonly MenuItemDefinitionSo _menuItem;
+        public MenuItemDefinitionSo MenuItem => _menuItem;
+        public bool IsCompleted { get; private set; }
+        public OrderItem(MenuItemDefinitionSo menuItem)
+        {
+            _menuItem = menuItem;
+            IsCompleted = false;
+        }
+        public bool CanBeFulfilledBy(MenuItemDefinitionSo menuItem)
+        {
+            if (IsCompleted)
+            {
+                return false;
+            }
+            if (menuItem == null)
+            {
+                return false;
+            }
+            if (MenuItem != menuItem)
+            {
+                return false;
+            }
+
+            return true;
+        }
+        public void MarkCompleted()
+        {
+            IsCompleted = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Order/OrderStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace Assets.Scripts.Order
+{
+    public enum OrderStatus
+    {
+        Active = 0,
+        Completed = 1,
+        Failed = 2
+    }
+}
