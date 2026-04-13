243aeaccd6b955cacff4f09242821ea1c241489f
w1r2s <donat.wirtus007@mail.ru>
2026-04-13 22:28:37 +0300

    Добавил level definition, список ассетов и LevelDatabase

---
+++ Assets/Scripts/ScriptableObjects/LevelDefinitionListSo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.ScriptableObjects
+{
+    [CreateAssetMenu(menuName = "KitchenChaos/Level Definition List")]
+    public class LevelDefinitionListSo : ScriptableObject
+    {
+        public List<LevelDefinitionSo> levels = new();
+    }
+}
