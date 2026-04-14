0ae88251bb454267b6fb805cc921fcdc179b0e94
w1r2s <donat.wirtus007@mail.ru>
2026-04-14 19:42:31 +0300

    Добавил OrderService и интерфейс IOrderService

---
+++ Assets/Scripts/Order/IOrderService.cs
@@ -0,0 +1,14 @@
+﻿using Assets.Scripts.ScriptableObjects;
+using System.Collections.Generic;
+
+namespace Assets.Scripts.Order
+{
+    public interface IOrderService
+    {
+        void Tick(float deltaTime);
+        ActiveOrder CreateOrder(IEnumerable<MenuItemDefinitionSo> items);
+        IReadOnlyList<ActiveOrder> GetActiveOrders();
+        void RemoveInactiveOrders();
+        bool TryFulfillOrderItem(MenuItemDefinitionSo menuItem);
+    }
+}
---
+++ Assets/Scripts/Order/OrderService.cs
@@ -0,0 +1,70 @@
+﻿using Assets.Scripts.ScriptableObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Assets.Scripts.Order
+{
+    public class OrderService : IOrderService
+    {
+        private readonly List<ActiveOrder> _orders;
+
+        public OrderService()
+        {
+            _orders = new List<ActiveOrder>();
+        }
+        public ActiveOrder CreateOrder(IEnumerable<MenuItemDefinitionSo> items)
+        {
+            if (items == null)
+               return null;
+         
+            var validItems = items.Where(item => item != null).ToList();
+
+            if( validItems.Count == 0 )
+                return null;
+
+            var order = new ActiveOrder(Guid.NewGuid().ToString(), validItems);
+
+            _orders.Add(order);
+
+            return order;
+        }
+
+        public IReadOnlyList<ActiveOrder> GetActiveOrders()
+        {
+            return _orders.Where(order => order.IsActive == true).ToList();
+        }
+
+        public void Tick(float deltaTime)
+        {
+            foreach (var order in _orders)
+            {
+                if(!order.IsActive) continue;
+
+                order.Tick(deltaTime);
+
+            }
+        }
+        public void RemoveInactiveOrders()
+        {
+            _orders.RemoveAll(order => !order.IsActive);
+        }
+
+        public bool TryFulfillOrderItem(MenuItemDefinitionSo menuItem)
+        {
+            if (menuItem == null)
+                return false;
+           
+            foreach (var order in _orders)
+            {
+                if (!order.IsActive)
+                    continue;
+
+                if (order.TryFulfill(menuItem))
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
