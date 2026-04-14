a2a6d20da563543722b8b314f88c8f994ef67ab9
w1r2s <donat.wirtus007@mail.ru>
2026-04-14 22:31:15 +0300

    Добавил OrderCounter для создания заказов через взаимодействие

---
+++ Assets/Scripts/Counters/OrderCounter.cs
@@ -0,0 +1,34 @@
+﻿using Assets.Scripts.Order;
+using Assets.Scripts.ScriptableObjects;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+namespace Assets.Scripts.Counters
+{
+    public class OrderCounter : BaseCounter
+    {
+        [SerializeField] private List<MenuItemDefinitionSo> orderItems;
+
+        private IOrderService _orderService;
+
+        [Inject]
+        private void Construct(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+        public override void Interact(Player player)
+        {
+          var order = _orderService.CreateOrder(orderItems);
+            if (order == null)
+            {
+                Debug.Log("Failed to create order: no valid menu items configured.");
+                return;
+            }
+            Debug.Log($"Order created:\n" +
+                $"order id: {order.Id},\n" +
+                $"order items count: {order.OrderItems.Count},\n" +
+                $"order max time: {order.MaxTime}");
+        }
+    }
+}
