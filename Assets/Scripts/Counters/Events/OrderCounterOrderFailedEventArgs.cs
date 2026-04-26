001a9b2df9222e7ff8aea84e357e1791884ab021
w1r2s <donat.wirtus007@mail.ru>
2026-04-26 17:17:59 +0300

    Добавил события OrderCounter для принятия и отказа при создании заказа

---
+++ Assets/Scripts/Counters/Events/OrderCounterOrderFailedEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using Assets.Scripts.Order;
+using System;
+
+namespace Assets.Scripts.Counters
+{
+    public class OrderCounterOrderFailedEventArgs : EventArgs
+    {
+        public OrderFlowFailureReason FailureReason { get; }
+
+        public OrderCounterOrderFailedEventArgs(OrderFlowFailureReason reason)
+        {
+            FailureReason = reason;
+        }
+    }
+}
--- Assets/Scripts/Counters/OrderCounter.cs
+++ Assets/Scripts/Counters/OrderCounter.cs
@@ -1,4 +1,5 @@
 ﻿using Assets.Scripts.Order;
+using System;
 using UnityEngine;
 using Zenject;
 
@@ -6,6 +7,9 @@ namespace Assets.Scripts.Counters
 {
     public class OrderCounter : BaseCounter
     {
+        public event EventHandler OnOrderAccepted;
+        public event EventHandler<OrderCounterOrderFailedEventArgs> OnOrderAcceptFailed;
+
         private IOrderFlowService _orderFlowService;
 
         [Inject]
@@ -16,11 +20,19 @@ namespace Assets.Scripts.Counters
         public override void Interact(Player player)
         {
             var orderResult = _orderFlowService.TryCreateOrder();
-            if(!orderResult.Success)
+            if (!orderResult.Success)
             {
+                if (orderResult.FailureReason.HasValue)
+                {
+                    OnOrderAcceptFailed?.Invoke(this, new OrderCounterOrderFailedEventArgs(orderResult.FailureReason.Value));
+                }
+
                 Debug.Log($"Failed to create order: {orderResult.FailureReason}");
                 return;
             }
+            OnOrderAccepted?.Invoke(this, EventArgs.Empty);
+
+
             Debug.Log($"Order created:\n" +
                 $"order id: {orderResult.Order.Id},\n" +
                 $"order items count: {orderResult.Order.OrderItems.Count},\n" +
