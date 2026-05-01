0e67fc621b38b7ed768cb1363699743015aacb95
w1r2s <donat.wirtus007@mail.ru>
2026-05-01 21:24:08 +0300

    Добавил управляемый CurrentLevelProvider и инициализацию стартового уровня

--- Assets/Scripts/Level/ICurrentLevelProvider.cs
+++ Assets/Scripts/Level/ICurrentLevelProvider.cs
@@ -1,9 +1,13 @@
 ﻿using Assets.Scripts.ScriptableObjects;
+using System;
 
 namespace Assets.Scripts.Level
 {
     public interface ICurrentLevelProvider
     {
         LevelDefinitionSo CurrentLevel {  get; }
+        bool TrySetCurrentLevel(int levelNumber);
+        bool TryMoveToNextLevel();
+        event EventHandler OnCurrentLevelChanged;
     }
 }
