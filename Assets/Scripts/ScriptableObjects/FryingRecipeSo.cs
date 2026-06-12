221ccf797318be8464863995bf4e97f1b95ef3e3
w1r2s <donat.wirtus007@mail.ru>
2026-06-13 00:26:30 +0300

    Удалил старый код и неиспользуемые ресурсы, нормализовал расположение активных ассетов

--- Assets/Scripts/ScriptableObjects/FryingRecipeSo.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu()]
-public class FryingRecipeSo : ProcessRecipeSo
-{
-    public float fryingTimerMax;
-    public override RecipeType Type => RecipeType.Frying;
-}
