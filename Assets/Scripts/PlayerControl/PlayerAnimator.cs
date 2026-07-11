cec073faa4ae9e4ee868ba58ba16b16b748586ec
w1r2s <donat.wirtus007@mail.ru>
2026-07-11 17:10:57 +0300

    Добавил исправленную модель персонажа и базовые анимации

---
+++ Assets/Scripts/PlayerControl/PlayerAnimator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace OrderRushKitchen.PlayerControl
+{
+    [RequireComponent(typeof(Player))]
+    public class PlayerAnimator : MonoBehaviour
+    {
+        [SerializeField] private Animator _animator;
+        private Player _player;
+
+        private static readonly int IsWalkingHash = Animator.StringToHash("IsWalking");
+
+        private void Awake()
+        {
+            _player = GetComponent<Player>();
+        }
+
+        private void LateUpdate()
+        {
+            _animator.SetBool(IsWalkingHash, _player.IsWalking());
+        }
+    }
+}
