ec4cca6fdb46fc43a0ebeeca4e5f8490c29796ca
w1r2s <donat.wirtus007@mail.ru>
2026-07-12 03:56:55 +0300

    Добавил анимацию резки

--- Assets/Scripts/Counters/Visuals/CuttingCounterVisual.cs
+++ Assets/Scripts/Counters/Visuals/CuttingCounterVisual.cs
@@ -2,31 +2,26 @@ using UnityEngine;
 
 namespace OrderRushKitchen.Counters
 {
-
     public class CuttingCounterVisual : MonoBehaviour
     {
-        private Animator animator;
+        [SerializeField] private Animator animator;
 
         [SerializeField] private CuttingCounter cuttingCounter;
 
-        private void Awake()
-        {
-            animator = GetComponent<Animator>();
-        }
+        private static readonly int cutHash = Animator.StringToHash("Cut");
         private void Start()
         {
             cuttingCounter.OnCut += CuttingCounter_OnCut;
-        }
-
-        private void CuttingCounter_OnCut(object sender, System.EventArgs e)
-        {
-            animator.SetTrigger("Cut");
         }
         private void OnDestroy()
         {
             if (cuttingCounter != null)
                 cuttingCounter.OnCut -= CuttingCounter_OnCut;
         }
-    }
 
+        private void CuttingCounter_OnCut(object sender, System.EventArgs e)
+        {
+            animator.SetTrigger(cutHash);
+        }
+    }
 }
