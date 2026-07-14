64e8057e861b9326e550e7617bca4e9321a2534d
w1r2s <donat.wirtus007@mail.ru>
2026-07-14 19:32:34 +0300

    Добавил частицы жарки для StoveCounter

--- Assets/Scripts/Counters/Visuals/StoveCounterVisual.cs
+++ Assets/Scripts/Counters/Visuals/StoveCounterVisual.cs
@@ -5,22 +5,35 @@ namespace OrderRushKitchen.Counters
 
     public class StoveCounterVisual : MonoBehaviour
     {
-        [SerializeField] private GameObject stoveOnGameObject;
-        [SerializeField] private GameObject particlesGameObject;
+
+        [SerializeField] private ParticleSystem fryingParticles;
 
         [SerializeField] private StoveCounter stoveCounter;
 
         private void Start()
         {
+            fryingParticles.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+
             stoveCounter.OnStateChanged += StoveCounter_OnStateChanged;
         }
 
+        private void OnDestroy()
+        {
+            if (stoveCounter != null)
+                stoveCounter.OnStateChanged -= StoveCounter_OnStateChanged;
+        }
         private void StoveCounter_OnStateChanged(object sender, StoveCounter.OnStateChangedEventArgs e)
         {
             bool showVisual = e.state == StoveCounter.State.Frying || e.state == StoveCounter.State.Fried;
-            stoveOnGameObject.SetActive(showVisual);
-            particlesGameObject.SetActive(showVisual);
+
+            if (showVisual && !fryingParticles.isPlaying)
+            {
+                fryingParticles.Play();
+            }
+            if (!showVisual && fryingParticles.isPlaying)
+            {
+                fryingParticles.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+            }
         }
     }
-
 }
